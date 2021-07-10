dff457cf27e4435070a0d5e733adc4dccdcf641b
hllbr <halibrahim.kocak@gmail.com>
2021-07-10 17:59:01 +0300

    Navigation_Concept

---
+++ app/src/main/java/com/hllbr/nasvigationkotlin/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.hllbr.nasvigationkotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        /*
+        Fragmentlar ile çalışırken navigation fragmenti ile çalışmak zaruri bir durum haline geldi
+        Navigation bir map navigasyonu değil uygulama navigasyonudur.
+        Uygulama içersinde hangi aktiviteden hangisine gidilebileceğini hangi animasyonları bu geçişler sırasında kullanabileceğimizi yönettiğmiz grandle gibi hem görsel hemde xml olarak işlemlerimizi gerçekleştirebildiğimiz bir yapı
+        Uygulama içerisindeki yaşam döngüsünü tasarladığımız alan olarak düşünebiliriz.
+        Fragmentlar ile çalışırken veri aktarımı gibi önemli işlemlerin gerçekleştirilmesi zorluk çıkarmaya başlayabiliyor.
+        --
+        Android Jetpack -->
+
+        Android tarafından geliştiricilerin uygulamalarını daha verimli hale getirebilmelerini sağlamak amacıyla oluşturulmuş bir kütüphane oalrak ifade edilebilir .
+        Bu yapı içerisinde bulunan ifadelerin bazıları zaten çok öncedende mevcuttu.
+        Fragmentler bu duruma örnek olarak verilebilir.
+        Android güçlenebilmek için kotlini bünyesine dahil etmesiyle bu yapının önemi arttı ve genişletildi diyebiliriz .
+
+
+        -->
+        sqlite-/webkit-/activity-/paging-/browser-/navigation JETPACK ailesinin bazı üyeleri olarak karşımıza çıkıyor.
+
+
+         */
+
+    }
+}
